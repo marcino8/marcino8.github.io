585283d9ce59ae7540a1abdd8a8db28e57fa7f90
marcino8 <mklimczak7@gmail.com>
2021-06-05 22:16:03 +0200

    update 1.4

--- google/vue.js
+++ google/vue.js
@@ -2,19 +2,33 @@ var app = new Vue({
     el: '#app',
     data: {
       googleSearch: '',
+	  textSearch:'',
 	  cities: window.cities,
     },
 
 	computed: {
                     filteredCities: function() {
+						
                         let results = this.cities.filter(city => city.name.includes(this.googleSearch));
-                        return results.slice(0,10);
+						if(this.googleSearch != '')
+							return  results.slice(0,10);
+						else return null;
+						
                     }
                 },
-				updated() {
+	updated() {
                     this.$nextTick(() => {
                         this.$refs.inputFocus.focus();
                     });
+                },
+	methods: {
+                    potwierdz() {
+                        this.textSearch = this.googleSearch
+                    },
+                    uzupelnij(miasto) {
+                        this.googleSearch = miasto.name;
+                        this.textSearch = this.googleSearch;
+                    },
                 }
 
   });
